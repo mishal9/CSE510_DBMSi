5f76ceca9426a6f094b769ee4432bdafa710c727
Monil <MONILnisar2012>
2021-04-14 17:26:59 -0700

    Work in progress.

--- Phase_3/src/iterator/GroupByWithHash.java
+++ Phase_3/src/iterator/GroupByWithHash.java
@@ -2,8 +2,19 @@ package iterator;
 
 import global.AggType;
 import global.AttrType;
+import heap.Tuple;
+
+import java.util.LinkedList;
 
 public class GroupByWithHash {
+    public static Tuple[] _result;
+    private static AttrType[] _attrType;
+    private static int _len_in;
+    private static boolean status = true;
+    private static short[] _attr_sizes;
+    private static AggType _agg_type;
+    private static int idx;
+    private int _n_pages;
 
     GroupByWithHash(
             AttrType[] in1, int len_in1, short[] t1_str_sizes,
@@ -15,6 +26,12 @@ public class GroupByWithHash {
             int n_out_flds,
             int n_pages
     ){
+        _attrType = in1;
+        _len_in = len_in1;
+        _attr_sizes = t1_str_sizes;
+        _agg_type = agg_type;
+        _n_pages = n_pages;
+        int buffer_pages = _n_pages/2;
 
     }
 }
